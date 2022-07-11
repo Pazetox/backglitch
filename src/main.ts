cf956726251e61de9a92ead82774ab0e32f5b4fa
Pazetox <pazetox@gmail.com>
2022-07-11 14:27:58 -0300

    teste
    teste

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  /*swagger*/
+
+  const config = new DocumentBuilder()
+    .setTitle('First App Swagger Hello World')
+    .setDescription('My hello world API description')
+    .setVersion('1.0')    
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
